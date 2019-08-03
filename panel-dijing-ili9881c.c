b368356b5cda939ccf2cddbe05e2f6abff010704
lambdadroid <lambdadroid@gmail.com>
2019-08-03 17:27:48 +0200

    Wrap long lines

--- panel-dijing-ili9881c.c
+++ panel-dijing-ili9881c.c
@@ -613,7 +613,9 @@ static int dijing_ili9881c_probe(struct mipi_dsi_device *dsi)
 
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE | MIPI_DSI_MODE_EOT_PACKET | MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM;
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
+			  MIPI_DSI_MODE_EOT_PACKET |
+			  MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM;
 
 	drm_panel_init(&ctx->panel);
 	ctx->panel.dev = dev;
